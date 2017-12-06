d1f1cb3b1ed5931e9754438545b9ea99ae7b95e0
Panos Kousidis <panos.kousidis@gmail.com>
2017-12-06 17:10:18 +0000

    automation

--- AOC/WebHelper.cs
+++ AOC/WebHelper.cs
@@ -10,20 +10,29 @@ namespace AoC
         {
             var filename = Path.Combine(Path.GetTempPath(), $"{year}{day:00}.aoc");
             if (File.Exists(filename))
-            {
                 return File.ReadAllText(filename);
-            }
-            else
-            {
-                var wb = new WebBrowser();
-                wb.Navigate($"http://adventofcode.com/{year}/day/{day}/input");
-                while (wb.ReadyState != WebBrowserReadyState.Complete) { Application.DoEvents(); }
-                var s = wb.Document?.Body?.InnerText;
-                if (s == null) return null;
-                if (s.Contains("HTTP 404")) return null;
-                File.WriteAllText(filename, s);
-                return s;
-            }
+
+            var wb = new WebBrowser();
+            wb.Navigate($"http://adventofcode.com/{year}/day/{day}/input");
+            while (wb.ReadyState != WebBrowserReadyState.Complete) { Application.DoEvents(); }
+            var s = wb.Document?.Body?.InnerText;
+            if (s == null) return null;
+            if (s.Contains("HTTP 404")) return null;
+            File.WriteAllText(filename, s);
+            return s;
         }
+
+        public static void SubmitAnswer(int year, int day, string answer, int part)
+        {
+            var wb = new WebBrowser();
+            wb.Navigate($"http://adventofcode.com/{year}/day/{day}/input");
+            while (wb.ReadyState != WebBrowserReadyState.Complete) { Application.DoEvents(); }
+
+            var eInput = wb.Document?.GetElementById($"part{part}");
+            eInput?.SetAttribute("value", answer);
+            var eSubmit = wb.Document?.GetElementById($"part{part}");
+            eSubmit?.InvokeMember("click");
+        }
+
     }
 }
--- DayLibraryTests/Program.cs
+++ DayLibraryTests/Program.cs
@@ -6,19 +6,39 @@ namespace DayLibraryTests
 {
     public class Program
     {
+        private const int Year = 2017;
+        private const int Day = 7;
+
+
+        [STAThread]
         public static void Main()
         {
-            const int year = 2017;
-            const int day = 7;
-
-            var input = WebHelper.GetInput(year, day);
-
-            var handle = Activator.CreateInstance("DayLibrary", $"DayLibrary.AoC{year}Day{day:00}");
+            var input = WebHelper.GetInput(Year, Day);
+            var handle = Activator.CreateInstance("DayLibrary", $"DayLibrary.AoC{Year}Day{Day:00}");
             var cDay = (DayBase)handle.Unwrap();
-            Console.WriteLine(cDay.Part1(input));
-            Console.WriteLine(cDay.Part2(input));
+
+            var part1Solution = cDay.Part1(input);
+            Console.WriteLine($"Part 1 Solution : {part1Solution}");
+            AskPartSubmission(1, part1Solution);
+            var part2Solution = cDay.Part2(input);
+            Console.WriteLine($"Part 2 Solution : {part2Solution}");
+            AskPartSubmission(2, part2Solution);
             Console.ReadLine();
         }
 
+        private static void AskPartSubmission(int part, string solution)
+        {
+            string k = null;
+            while (k != "y" && k != "n")
+            {
+                Console.Write($"Submit Part {part} Solution? [Y/N] : ");
+                k = Console.ReadKey().KeyChar.ToString().ToLower();
+                Console.WriteLine();
+            }
+            if (k != "y") return;
+
+            Console.WriteLine("Submitting answer...");
+            WebHelper.SubmitAnswer(Year, Day, solution, part);
+        }
     }
 }
