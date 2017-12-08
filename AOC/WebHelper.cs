6f1020fb958258baf8c3d627d1c0727d94552945
Panos Kousidis <panos.kousidis@gmail.com>
2017-12-08 00:28:45 +0000

    WebHelper login to site

--- AOC/WebHelper.cs
+++ AOC/WebHelper.cs
@@ -5,19 +5,36 @@ namespace AoC
 {
     public class WebHelper
     {
-        
+
         public static string GetInput(int year, int day)
         {
             var filename = Path.Combine(Path.GetTempPath(), $"{year}{day:00}.aoc");
             if (File.Exists(filename))
                 return File.ReadAllText(filename);
-
             var wb = new WebBrowser();
-            wb.Navigate($"http://adventofcode.com/{year}/day/{day}/input");
-            while (wb.ReadyState != WebBrowserReadyState.Complete) { Application.DoEvents(); }
-            var s = wb.Document?.Body?.InnerText;
-            if (s == null) return null;
-            if (s.Contains("HTTP 404")) return null;
+            string s;
+            while (true)
+            {
+                wb.Navigate($"http://adventofcode.com/{year}/day/{day}/input");
+                while (wb.ReadyState != WebBrowserReadyState.Complete &&
+                       wb.ReadyState != WebBrowserReadyState.Interactive)
+                {
+                    Application.DoEvents();
+                }
+                s = wb.Document?.Body?.InnerText;
+                if (s == null) return null;
+                if (!s.Contains("HTTP 40"))
+                {
+                    break;
+                }
+                var frm = new Form();
+                frm.Controls.Add(wb);
+                wb.Dock = DockStyle.Fill;
+                wb.Navigate("http://adventofcode.com");
+                frm.WindowState = FormWindowState.Maximized;
+                MessageBox.Show("Please login and then close the window");
+                frm.ShowDialog();
+            }
             File.WriteAllText(filename, s);
             return s;
         }
