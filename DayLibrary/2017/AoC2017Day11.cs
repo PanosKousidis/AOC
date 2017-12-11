14d8561aa32e650fcc7664203ac95454dea66d99
Panos Kousidis <panos.kousidis@gmail.com>
2017-12-11 18:53:13 +0000

    Day 11

--- DayLibrary/2017/AoC2017Day11.cs
+++ DayLibrary/2017/AoC2017Day11.cs
@@ -1,25 +1,71 @@
+using System;
+using System.Drawing;
+using System.Xml.Linq;
+
 namespace DayLibrary
 {
     public class AoC2017Day11 : DayBase
     {
+        private static int _furthest;
 
         public override string Part1(string input)
         {
-            return Part1(input, null);
-        }
-        public override string Part2(string input)
-        {
-            return Part2(input, null);
+            var location = GetCurrentLocation(input);
+            return GetDistance(location).ToString();
         }
 
-        public override string Part1(string input, object args)
+        public override string Part2(string input)
         {
-            return null;
+            GetCurrentLocation(input);
+            return _furthest.ToString();
         }
+
+        private static Point GetCurrentLocation(string input)
+        {
+            var x = 0;
+            var y = 0;
+            _furthest = 0;
+            var directions = input.Split(',');
+
+            foreach (var direction in directions)
+            {
+                switch (direction)
+                {
+                    case "s":
+                        y -= 2;
+                        break;
+                    case "se":
+                        y--;
+                        x++;
+                        break;
+                    case "sw":
+                        y--;
+                        x--;
+                        break;
+                    case "n":
+                        y += 2;
+                        break;
+                    case "ne":
+                        y++;
+                        x++;
+                        break;
+                    case "nw":
+                        y++;
+                        x--;
+                        break;
+                    default: throw new NotSupportedException();
+                }
+                var d = GetDistance(new Point(x, y));
+                if (d > _furthest) _furthest = d;
+            }
+            return new Point(x,y);
+        }
+
+        private static int GetDistance(Point p)
+        {
+            return Math.Abs(Math.Abs(p.X) + (Math.Abs(p.Y) - Math.Abs(p.X) > 0 ? Math.Abs(p.Y) - Math.Abs(p.X) / 2 : 0));
+        }
+
        
-        public override string Part2(string input, object args)
-        {
-            return null;
-        }
     }
 }
--- DayLibraryTests/2017/AoC2017Day11Test.cs
+++ DayLibraryTests/2017/AoC2017Day11Test.cs
@@ -10,14 +10,17 @@ namespace DayLibraryTests
         [Test]
         public void TestPart1()
         {
-            Assert.AreEqual(null, TestPart1(Input));
-            //Assert.AreEqual(null, TestPart1(null));
+            Assert.AreEqual("3", TestPart1("ne,ne,ne"));
+            Assert.AreEqual("0", TestPart1("ne,ne,sw,sw"));
+            Assert.AreEqual("2", TestPart1("ne,ne,s,s"));
+            Assert.AreEqual("3", TestPart1("se,sw,se,sw,sw"));
+            Assert.AreEqual("784", TestPart1(null));
+            Assert.AreEqual("6", TestPart1("se,ne,se,ne,se,ne"));
         }
         [Test]
         public void TestPart2()
         {
-            Assert.AreEqual(null, TestPart2(Input));
-            //Assert.AreEqual(null, TestPart2(null));
+            Assert.AreEqual("1558", TestPart2(null));
         }
     }
 }
