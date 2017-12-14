21ca2c61e2d3b12ff3b3ce3d047723e301909d64
Panos Kousidis <panos.kousidis@gmail.com>
2017-12-14 17:43:30 +0000

    Day 13 added

--- DayLibrary/2017/AoC2017Day13.cs
+++ DayLibrary/2017/AoC2017Day13.cs
@@ -1,25 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace DayLibrary
 {
     public class AoC2017Day13 : DayBase
     {
-
         public override string Part1(string input)
         {
-            return Part1(input, null);
-        }
-        public override string Part2(string input)
-        {
-            return Part2(input, null);
+            var scanners = GetScanners(input);
+            var max = scanners.Max(scanner => scanner.Depth);
+            var score = 0;
+
+            for (var i = 0; i <= max; i++)
+            {
+                var scanner2 = scanners.FirstOrDefault(s => s.Depth == i);
+                if (scanner2 != null && scanner2.Index == 0)
+                {
+                    score += scanner2.Depth * scanner2.Range;
+                }
+                foreach (var scanner in scanners)
+                {
+                    scanner.Move();
+                }
+            }
+            return score.ToString();
         }
 
-        public override string Part1(string input, object args)
+        public override string Part2(string input)
         {
-            return null;
+            var time = 0;
+            var scanners = GetScanners(input);
+            var max = scanners.Max(scanner => scanner.Depth);
+            var movers = new Dictionary<int, int>();
+
+            for (; ; time++)
+            {
+                movers.Add(time, 0);
+                var moversCopy = movers.ToList();
+                foreach (var m in moversCopy)
+                {
+                    var scanner2 = scanners.FirstOrDefault(s => s.Depth == m.Value);
+                    if (scanner2 != null && scanner2.Index == 0)
+                    {
+                        movers.Remove(m.Key);
+                        continue;
+                    }
+                    movers[m.Key]++;
+                    if (movers.ContainsKey(m.Key) && m.Value == max)
+                        return (m.Key).ToString();
+                }
+                MoveScanners(scanners, 1);
+            }
         }
-       
-        public override string Part2(string input, object args)
+
+        private static Scanner[] GetScanners(string input)
         {
-            return null;
+            return input.Split(new[] {"\r\n"}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(line => Regex.Match(line, "(\\d*):\\s(\\d*)"))
+                .Select(match => new Scanner {  Range = int.Parse(match.Groups[2].Value),
+                                                Depth = int.Parse(match.Groups[1].Value)
+                                             })
+                .ToArray();
+        }
+        
+        private static void MoveScanners(Scanner[] scanners,int repeat)
+        {
+            for (var i = 0; i < repeat; i++)
+            {
+                foreach (var s in scanners) s.Move();
+            }
+        }
+    }
+
+    public class Scanner
+    {
+        public int Depth { get; set; } = -1;
+        public int Index { get; private set; }
+        public int Range { get; set; } = -1;
+        private bool GoingDown { get; set; } = true;
+
+        public void Move()
+        {
+            if (Range==-1) return;
+
+            if (GoingDown)
+            {
+                Index++;
+                if (Index == Range - 1) GoingDown = false;
+            }
+            else
+            {
+                Index--;
+                if (Index == 0) GoingDown = true;
+            }
         }
     }
 }
--- DayLibraryTests/2017/AoC2017Day13Test.cs
+++ DayLibraryTests/2017/AoC2017Day13Test.cs
@@ -5,19 +5,22 @@ namespace DayLibraryTests
     [TestFixture]
     public class AoC2017Day13Test : DayTest
     {
-        private const string Input = @"";
+        private const string Input = @"0: 3
+1: 2
+4: 4
+6: 4";
 
         [Test]
         public void TestPart1()
         {
-            Assert.AreEqual(null, TestPart1(Input));
-            //Assert.AreEqual(null, TestPart1(null));
+            Assert.AreEqual("24", TestPart1(Input));
+            Assert.AreEqual(null, TestPart1(null));
         }
         [Test]
         public void TestPart2()
         {
-            Assert.AreEqual(null, TestPart2(Input));
-            //Assert.AreEqual(null, TestPart2(null));
+            Assert.AreEqual("10", TestPart2(Input));
+            Assert.AreEqual("3921270", TestPart2(null));
         }
     }
 }
